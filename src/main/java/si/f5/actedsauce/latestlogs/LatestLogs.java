c6ed35a4a42b973c1f79d64134963c1c2ac84a4f
Acted <t-913.saki@outlook.jp>
2024-09-04 22:31:08 +0900

    first commit

---
+++ src/main/java/si/f5/actedsauce/latestlogs/LatestLogs.java
@@ -0,0 +1,17 @@
+package si.f5.actedsauce.latestlogs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LatestLogs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
